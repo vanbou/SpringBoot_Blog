158c4a7f8e40fe39d0a6e86db1673c774f146fe0
vanbou <resumebb@163.com>
2022-04-07 10:41:39 +0800

    v1.0

---
+++ src/main/java/com/site/blog/my/core/config/Constants.java
@@ -0,0 +1,14 @@
+package com.site.blog.my.core.config;
+/**
+ * Description :
+ * Created by Resumebb
+ * Date :2022/3/30
+ */
+
+
+/**
+ * 配置上传文件的默认url前缀，根据部署设置自行修改
+ */
+public class Constants {
+    public final static String FILE_UPLOAD_DIC = "D:\\fileImg\\";
+}
---
+++ src/main/java/com/site/blog/my/core/config/MyBlogWebMvcConfigurer.java
@@ -0,0 +1,34 @@
+package com.site.blog.my.core.config;
+
+import com.site.blog.my.core.interceptor.AdminLoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/**
+ * Description :
+ * Created by Resumebb
+ * Date :2022/3/30
+ */
+
+/*
+    管理员后台系统资源配置拦截器
+ */
+@Configuration
+public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private AdminLoginInterceptor adminLoginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 添加一个拦截器，拦截以/admin为前缀的url路径
+        registry.addInterceptor(adminLoginInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/login").excludePathPatterns("/admin/dist/**").excludePathPatterns("/admin/plugins/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:" + Constants.FILE_UPLOAD_DIC);
+    }
+}
